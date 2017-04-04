3cc31f9f4ab6ad1bd0d2aa01d634f2b32ec9fae0
226238 <226238@student.pwr.wroc.pl>
2017-04-04 17:00:40 +0200

    stos dziala

--- lab4/src/stack.cpp
+++ lab4/src/stack.cpp
@@ -1,5 +1,6 @@
 #include "stack.hh"
-#include "iostream"
+#include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -9,32 +10,92 @@ using namespace std;
 
 stack::stack()
 {
-	//SIZE = 10;
-	//_stack[SIZE];
+	SIZE = 0;
+	_stack = new int[SIZE];
 }
 
 stack::~stack()
 {
-
+	delete [] _stack;
 }
 
 void stack::push()
 {
-	//int element = 1;
+	SIZE++;
 
+	int * new_stack = new int [SIZE];
+
+	for(int i=0 ; i < (SIZE-1) ; i++)
+	{
+		new_stack[i] = _stack[i];    
+	}
+
+	delete [] _stack;
+
+	_stack = new_stack;
+
+	_stack[SIZE-1] = SIZE;
 }
 
 void stack::pop()
 {
+	if(SIZE > 0)
+	{
+		int * new_stack = new int [SIZE-1];
 
+		for(int i=0 ; i < (SIZE-1) ; i++)
+		{
+			new_stack[i] = _stack[i];    
+		}
+
+		delete [] _stack;
+
+		_stack = new_stack;
+
+		SIZE--;
+	}
+
+	else
+	{
+		cout << "Stos pusty!" << endl;
+	}
 }
 
 void stack::size()
 {
+	cout << "Aktualna ilosc elementow na stosie to: " << SIZE << endl;
 
+	for(int i=0 ; i < SIZE ; i++)
+	{
+		cout << _stack[i] << " ";
+	}
+
+	cout << endl;
 }
 
 void stack::run()
 {
-	cout << "stack" << endl;
+	int ile;
+
+	cout << "Ile elementow dodac: ";
+	cin >> ile;
+
+	size();
+
+	for(int i=0 ; i < ile ; i++)
+	{
+		push();
+	}
+
+	size();
+
+	cout << "Ile elementow zdjac: ";
+	cin >> ile;
+
+	for(int i=0 ; i < ile ; i++)
+	{
+		pop();
+	}
+
+	size();
 }
